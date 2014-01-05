f8a7e440ffbe03bd1653d70c47407e62e0fb03bf
Kouta Yoshimura <quetz.design@gmail.com>
2014-01-05 16:53:44 +0900

    転写ミスを修正

--- app.js
+++ app.js
@@ -5,56 +5,48 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
+var tasks = require('./routes/tasks');
 var http = require('http');
 var path = require('path');
 var mongoskin = require('mongoskin');
-
+var db = mongoskin.db('mongodb://localhost:27017/todo?auto_reconnect', {safe:true});
 var app = express();
-var db = mongoskin.db('mongodb://localhost:27017/todo?auto_reconnect', { safe:true });
-
-// all environments
 app.use(function(req, res, next) {
 	req.db = {};
 	req.db.tasks = db.collection('tasks');
 	next();
-});
+})
+app.locals.appname = 'Express.js Todo App'
+// all environments
 
-app.locals.appname = 'Express.js Todo App';
+
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser());
-app.use(express.session({
-	secret: '59B93087-78BC-4EB9-993A-A61FC844F6C9'
-}));
+app.use(express.session({secret: '59B93087-78BC-4EB9-993A-A61FC844F6C9'}));
 app.use(express.csrf());
-app.use(require('less-middleware')({
-	src: __dirname + '/public',
-	compress: true
-}));
 
+app.use(require('less-middleware')({ src: __dirname + '/public', compress: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
 	res.locals._csrf = req.session._csrf;
 	return next();
-});
-
+})
 app.use(app.router);
 
-if('development' == app.get('env')) {
+// development only
+if ('development' == app.get('env')) {
 	app.use(express.errorHandler());
 }
-
-app.param('task_id', function(req, res, next, task_id){
+app.param('task_id', function(req, res, next, taskId) {
 	req.db.tasks.findById(taskId, function(error, task){
-		if(error) return next(error);
-		if(!task) return next(new Error('task is not found.'));
-
+		if (error) return next(error);
+		if (!task) return next(new Error('Task is not found.'));
 		req.task = task;
 		return next();
 	});
@@ -62,15 +54,15 @@ app.param('task_id', function(req, res, next, task_id){
 
 app.get('/', routes.index);
 app.get('/tasks', tasks.list);
-app.post('/tasks', tasks.markAllCompleted);
-app.post('/tasks', task.add);
+app.post('/tasks', tasks.markAllCompleted)
+app.post('/tasks', tasks.add);
 app.post('/tasks/:task_id', tasks.markCompleted);
 app.del('/tasks/:task_id', tasks.del);
 app.get('/tasks/completed', tasks.completed);
+
 app.all('*', function(req, res){
 	res.send(404);
-});
-
+})
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express Todo App' });
+	res.render('index', { title: 'Express.js Todo App' });
 };
